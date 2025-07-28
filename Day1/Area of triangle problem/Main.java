b09aa822a806073fd365f7246aea537a8d4cb6f1
kavitha293 <bandakavitha40@gmail.com>
2025-07-28 18:55:08 +0530

    Update Main.java

--- Day1/Area of triangle problem/Main.java
+++ Day1/Area of triangle problem/Main.java
@@ -1,12 +1,4 @@
-/******************************************************************************
 
-Welcome to GDB Online.
-GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
-C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
-Code, Compile, Run and Debug online from anywhere in world.
-
-*******************************************************************************/
-//find the area of triangle using herons formula;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.IOException;
